278531043b45a160ba47a4f0a74f7eaf755a418a
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-08-28 19:05:59 +0600

    M pages/api/index.ts  D src/utils/getTemplate.ts ?? src/utils/getHTML.ts

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,47 +1,12 @@
-import { Data } from '@/src/features/Input'
-import getTemplate from '@/src/utils/getTemplate'
-
 import puppeteer from 'puppeteer-core'
 import chromium from '@sparticuz/chromium'
+import getHTML from '@/src/utils/getHTML'
 
 chromium.setHeadlessMode = true
 chromium.setGraphicsMode = false
 
 export default async (req: any, res: any) => {
-  const template = getTemplate()
-  const html = template.replace(
-    '<!-- CONTENT -->',
-    Object.entries(req.body as Data)
-      .map(([name, subjects]) =>
-        subjects.length
-          ? `
-        <section>
-          <h3>${name}</h3>
-
-          <table>
-            <tbody>
-              <tr>
-                <th>সময়</th>
-                <th>বিষয়</th>
-                <th>রুম</th>
-              </tr>
-              ${subjects
-                .map(
-                  ({ startTime, name, room }) => `
-                    <tr>
-                      <td>${startTime}</td>
-                      <td>${name}</td>
-                      <td>${room}</td>
-                    </tr>`
-                )
-                .join('')}
-            </tbody>
-          </table>
-        </section>`
-          : ''
-      )
-      .join('')
-  )
+  const html = getHTML(req.body)
 
   const browser = await puppeteer.launch({
     args: chromium.args,
--- src/utils/getHTML.ts
+++ src/utils/getHTML.ts
@@ -1,5 +1,7 @@
-export default function () {
-  return `<!DOCTYPE html>
+import { Data } from '../features/Input'
+
+export default function (data: Data) {
+  const template = `<!DOCTYPE html>
   <html lang="en">
     <head>
       <meta charset="UTF-8" />
@@ -114,4 +116,38 @@ export default function () {
     </body>
   </html>
   `
+
+  return template.replace(
+    '<!-- CONTENT -->',
+    Object.entries(data)
+      .map(([name, subjects]) =>
+        subjects.length
+          ? `
+        <section>
+          <h3>${name}</h3>
+
+          <table>
+            <tbody>
+              <tr>
+                <th>সময়</th>
+                <th>বিষয়</th>
+                <th>রুম</th>
+              </tr>
+              ${subjects
+                .map(
+                  ({ startTime, name, room }) => `
+                    <tr>
+                      <td>${startTime}</td>
+                      <td>${name}</td>
+                      <td>${room}</td>
+                    </tr>`
+                )
+                .join('')}
+            </tbody>
+          </table>
+        </section>`
+          : ''
+      )
+      .join('')
+  )
 }
