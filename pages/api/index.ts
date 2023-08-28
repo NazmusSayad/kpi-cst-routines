37bbd0cc608926a05ae55bc90de688a16b0f2941
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-08-28 14:24:20 +0600

    M package.json ?? pages/

---
+++ pages/api/index.ts
@@ -0,0 +1,57 @@
+import { Data } from '@/src/features/Input'
+import getTemplate from '@/src/utils/getTemplate'
+import PCR from 'puppeteer-chromium-resolver'
+
+export default async (req: any, res: any) => {
+  const template = getTemplate()
+  const html = template.replace(
+    '<!-- CONTENT -->',
+    Object.entries(req.body as Data)
+      .map(([name, subjects]) =>
+        subjects.length
+          ? `
+        <section>
+          <h3>${name}</h3>
+
+          <table>
+            <tbody>
+              <tr>
+                <th>সময়</th>
+                <th>বিষয়</th>
+                <th>রুম</th>
+              </tr>
+              ${subjects
+                .map(
+                  ({ startTime, name, room }) => `
+                    <tr>
+                      <td>${startTime}</td>
+                      <td>${name}</td>
+                      <td>${room}</td>
+                    </tr>`
+                )
+                .join('')}
+            </tbody>
+          </table>
+        </section>`
+          : ''
+      )
+      .join('')
+  )
+
+  const stats = await PCR()
+  const browser = await stats.puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-gpu'],
+    executablePath: stats.executablePath,
+  })
+  const page = await browser.newPage()
+  await page.setViewport({ width: 1920, height: 1080 })
+  await page.setContent(html)
+
+  const element = await page.$('main')
+  const output =
+    element && (await element.screenshot({ encoding: 'base64', type: 'png' }))
+
+  res.send('data:image/png;base64,' + output)
+  await browser.close()
+}
