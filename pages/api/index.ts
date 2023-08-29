cf9266fd650693deb5c40c0e71ddf73cc3bab2c2
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-08-29 21:14:18 +0600

    M pages/api/index.ts

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -2,9 +2,60 @@ import puppeteer from 'puppeteer-core'
 import chromium from '@sparticuz/chromium'
 import getHTML from '@/src/utils/getHTML'
 
+const args = [
+  // '--allow-pre-commit-input',
+  // '--disable-background-networking',
+  // '--disable-background-timer-throttling',
+  // '--disable-backgrounding-occluded-windows',
+  // '--disable-breakpad',
+  // '--disable-client-side-phishing-detection',
+  // '--disable-component-extensions-with-background-pages',
+  // '--disable-component-update',
+  // '--disable-default-apps',
+  // '--disable-dev-shm-usage',
+  // '--disable-extensions',
+  // '--disable-hang-monitor',
+  // '--disable-ipc-flooding-protection',
+  // '--disable-popup-blocking',
+  // '--disable-prompt-on-repost',
+  // '--disable-renderer-backgrounding',
+  // '--disable-sync',
+  // '--enable-automation',
+  // '--enable-blink-features=IdleDetection',
+  // '--export-tagged-pdf',
+  // '--force-color-profile=srgb',
+  // '--metrics-recording-only',
+  // '--no-first-run',
+  // '--password-store=basic',
+  // '--use-mock-keychain',
+  // '--disable-domain-reliability',
+  // '--disable-print-preview',
+  // '--disable-speech-api',
+  // '--disk-cache-size=33554432',
+  // '--mute-audio',
+  '--no-default-browser-check',
+  '--no-pings',
+  '--single-process',
+  // '--disable-features=Translate,BackForwardCache,AcceptCHFrame,MediaRouter,OptimizationHints,AudioServiceOutOfProcess,IsolateOrigins,site-per-process',
+  // '--enable-features=NetworkServiceInProcess2,SharedArrayBuffer',
+  // '--hide-scrollbars',
+  // '--ignore-gpu-blocklist',
+  // '--in-process-gpu',
+  '--window-size=1920,1080',
+  // '--use-gl=angle',
+  // '--use-angle=swiftshader',
+  // '--allow-running-insecure-content',
+  '--disable-setuid-sandbox',
+  // '--disable-site-isolation-trials',
+  // '--disable-web-security',
+  '--no-sandbox',
+  // '--no-zygote',
+  "--headless='new'",
+]
+
 export default async (req: any, res: any) => {
   const browser = await puppeteer.launch({
-    args: chromium.args,
+    args: args,
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath(),
     headless: true,
@@ -12,7 +63,7 @@ export default async (req: any, res: any) => {
 
   const page = await browser.newPage()
   await page.setViewport({ width: 1920, height: 1080 })
-  await page.setContent(getHTML(req.body))
+  await page.setContent(getHTML(req.body || {}))
 
   const element = await page.$('main')
   const output =
