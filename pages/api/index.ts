8128a6d86fcba298a86883e4010bc302de3d9e63
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-08-30 21:47:00 +0600

    D fonts.css  M pages/api/index.ts  D src/Siyamrupali.ttf  M src/config.ts

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,23 +1,12 @@
 import puppeteer from 'puppeteer-core'
 import chromium from '@sparticuz/chromium'
 import getHTML from '@/src/utils/getHTML'
-
-import rubikNormal from '!!file-loader!@/src/fonts/rubik/Rubik-Regular.ttf'
-import rubikMedium from '!!file-loader!@/src/fonts/rubik/Rubik-Medium.ttf'
-import rubikBold from '!!file-loader!@/src/fonts/rubik/Rubik-Bold.ttf'
-
-import notoNormal from '!!file-loader!@/src/fonts/noto-sans-bangla/NotoSansBengali-Regular.ttf'
-import notoMedium from '!!file-loader!@/src/fonts/noto-sans-bangla/NotoSansBengali-Medium.ttf'
-import notoBold from '!!file-loader!@/src/fonts/noto-sans-bangla/NotoSansBengali-Bold.ttf'
+import { fonts } from '@/src/config'
 
 export default async (req: any, res: any) => {
-  await chromium.font(rubikNormal)
-  await chromium.font(rubikMedium)
-  await chromium.font(rubikBold)
-
-  await chromium.font(notoNormal)
-  await chromium.font(notoMedium)
-  await chromium.font(notoBold)
+  for (let font of fonts) {
+    await chromium.font(font)
+  }
 
   const browser = await puppeteer.launch({
     args: chromium.args,
